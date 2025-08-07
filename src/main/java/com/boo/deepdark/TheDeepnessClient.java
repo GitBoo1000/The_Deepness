3823324586413efc6fb170bf4ed84071c3cbf66c
GitBoo1000 <Boomobear@outlook.com>
2025-08-06 21:51:19 -0400

    Initial commit

---
+++ src/main/java/com/boo/deepdark/TheDeepnessClient.java
@@ -0,0 +1,10 @@
+package com.boo.deepdark;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheDeepnessClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/boo/deepdark/TheDeepnessDataGenerator.java
@@ -0,0 +1,11 @@
+package com.boo.deepdark;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheDeepnessDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
